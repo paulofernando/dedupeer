3ddd3b2f5720e8c01752ea4b71bda388d2440e37
paulofernando <pf@paulofernando.net.br>
2012-09-04 16:38:51 -0300

    ...

--- src/deduplication/checksum/RollingAlder32.java
+++ src/deduplication/checksum/RollingAlder32.java
@@ -1,5 +1,24 @@
 package deduplication.checksum;
 
-public class RollingAlder32 {
+import java.util.zip.Adler32;
+import java.util.zip.Checksum;
 
+public class RollingAlder32 {
+	
+	public static void rollingIn(byte[] data, long offset, int window) {
+		
+		
+		Checksum checksum = new Adler32();        
+        checksum.update(data, 0, bytes.length);
+       
+        /*
+         * Get the generated checksum using
+         * getValue method of Adler32 class.
+         */
+        long lngChecksum = checksum.getValue();
+		
+		
+		System.out.println();
+	}
+	
 }
