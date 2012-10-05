0e0d6a83dbf01270c602fe8b134b30a02cc54812
Paulo Fernando <pf@paulofernando.net.br>
2012-10-05 00:35:36 -0300

    Criação da classe para operações nos dados salvos sobre chunks

---
+++ src/deduplication/dao/operation/ChunkOperations.java
@@ -0,0 +1,13 @@
+package deduplication.dao.operation;
+
+import org.apache.cassandra.thrift.*;
+
+public class ChunkOperations {
+	
+	public void insertRow(String key, String Adler32, String name, String index, String length) {
+		//Clock clock = new Clock(System.nanoTime());
+		long time = System.nanoTime();		
+		
+	}
+	
+}
