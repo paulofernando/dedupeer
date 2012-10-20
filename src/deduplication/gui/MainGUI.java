2496823974b18d10c679fa2e145a0179b442ec69
Paulo Fernando <pf@paulofernando.net.br>
2012-10-19 21:48:40 -0300

    Adição do LookAndFell e tabela.

--- src/deduplication/gui/MainGUI.java
+++ src/deduplication/gui/MainGUI.java
@@ -3,11 +3,18 @@ package deduplication.gui;
 import java.awt.Toolkit;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+import org.pushingpixels.substance.api.skin.SubstanceGraphiteAquaLookAndFeel;
 
 import deduplication.gui.component.MainPanel;
 
 public class MainGUI extends JFrame {
-		
+	
+	private final int width = 500;
+	private final int height = 500;
+	
 	public MainGUI() {
 		this.setTitle("Dedupeer");
 		setup();
@@ -17,9 +24,32 @@ public class MainGUI extends JFrame {
 	}
 	
 	private void setup() {
-		this.setSize(400, 500);
+		this.setSize(width, height);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);	    
 	    setLocation((Toolkit.getDefaultToolkit().getScreenSize().width - this.getSize().width)>>1, 
 	    		(Toolkit.getDefaultToolkit().getScreenSize().height - this.getSize().height)>>1);
+	    
+	    lookAndFeel();
+	}
+
+	
+
+	/**
+	 * Sets up the look and feel to the application
+	 */
+	private void lookAndFeel() {
+		
+	    SwingUtilities.invokeLater(new Runnable() {
+	      public void run() {
+	        try {
+	          JFrame.setDefaultLookAndFeelDecorated(true);
+	          //UIManager.setLookAndFeel("org.pushingpixels.substance.api.skin.SubstanceGraphiteLookAndFeel");
+	          UIManager.setLookAndFeel("org.pushingpixels.substance.api.skin.SubstanceBusinessBlackSteelLookAndFeel");
+	        } catch (Exception e) {
+	          System.out.println("Substance failed to initialize");
+	        }
+	      }
+	    });
+	    
 	}
 }
--- src/deduplication/gui/component/MainPanel.java
+++ src/deduplication/gui/component/MainPanel.java
@@ -2,27 +2,36 @@ package deduplication.gui.component;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.FontMetrics;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
 
 public class MainPanel extends JPanel {
 	
 	private JButton btLogin, btAdd;
 	private JPanel groupButtons = new JPanel();
 	private BorderLayout borderLayout = new BorderLayout();
+	
+	private JTable table;
+	String[] columnNames = {"File",
+			"Progress",
+            "Storage economy",
+            "Restore"};
+		
 	public MainPanel() {
 		initComponents();
 		
 		this.setLayout(borderLayout);		
 		this.add(groupButtons, BorderLayout.PAGE_START);
 
-		JButton button = new JButton("Button 2 (CENTER)");
-		//button.setPreferredSize(new Dimension(200, 100));
-		this.add(button, BorderLayout.CENTER);
+		createAndAddTable();
 
-		button = new JButton("Long-Named Button 4 (PAGE_END)");
+		JButton button = new JButton("Long-Named Button 4 (PAGE_END)");
 		this.add(button, BorderLayout.PAGE_END);
 	}
 	
@@ -34,6 +43,25 @@ public class MainPanel extends JPanel {
 		
 		groupButtons.add(btLogin);
 		groupButtons.add(btAdd);
+				
+	}
+	
+	private void createAndAddTable() {
+		table = new JTable(new String[5][4], columnNames);
+		table.setFillsViewportHeight(true);
+		table.setShowVerticalLines(false);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setRowHeight(23);
 		
+		FontMetrics fontMetrics = this.getFontMetrics(table.getFont());
+		for(int i = 1; i < columnNames.length; i++) {		
+			table.getColumnModel().getColumn(i).setMaxWidth(fontMetrics.stringWidth(columnNames[i]) + 20);
+			table.getColumnModel().getColumn(i).setMinWidth(fontMetrics.stringWidth(columnNames[i]) + 20);
+		}
+		
+		
+		
+		JScrollPane scrollPane = new JScrollPane(table);
+		this.add(scrollPane, BorderLayout.CENTER);
 	}
 }
