6dd36153104ead587b43a2d2dcab7ff09aedddb2
Paulo Fernando <pf@paulofernando.net.br>
2012-10-27 12:14:14 -0300

    Adição das diferentes cores no JProgressBar

--- src/deduplication/gui/component/renderer/JProgressRenderer.java
+++ src/deduplication/gui/component/renderer/JProgressRenderer.java
@@ -2,7 +2,10 @@ package deduplication.gui.component.renderer;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.GradientPaint;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
 
 import javax.swing.JProgressBar;
 import javax.swing.JTable;
@@ -11,8 +14,14 @@ import javax.swing.table.TableCellRenderer;
 public class JProgressRenderer extends JProgressBar implements TableCellRenderer {
 
 	private static final long serialVersionUID = -4116117091520144073L;
-	private Color background, foreground;
+	private Color background;
 	private ProgressInfo progressInfo;
+	
+	Color[][] colors = new Color[][]{
+			{new Color(0xa4dcb2), new Color(0x61e868)},
+			{new Color(0xc0cde0), new Color(0x76a1e3)},
+			{new Color(0xe8e6dd), new Color(0xe8c53a)}			
+	};
 	
 	@Override
 	public Component getTableCellRendererComponent(JTable table, Object value,
@@ -36,7 +45,15 @@ public class JProgressRenderer extends JProgressBar implements TableCellRenderer
 			graphics.setColor(background);
 			graphics.fillRect(0, 0, this.getWidth(), this.getHeight());
 		} else {
-			graphics.drawString(progressInfo.getTypeString(), (this.getWidth()>>1) - (graphics.getFontMetrics().stringWidth(progressInfo.getTypeString())), ((this.getHeight()>>1) - (graphics.getFontMetrics().getHeight()>>1)));
+			Graphics2D g2 = (Graphics2D) graphics;
+	        Rectangle2D r = new Rectangle2D.Double(2, 1, (progressInfo.getProgress() * this.getWidth()) / 100, this.getHeight() - 2);
+	        GradientPaint gp = new GradientPaint(0, 0, colors[progressInfo.getType()][0], 0, this.getHeight(),
+	        		colors[progressInfo.getType()][1], true);
+	        g2.setPaint(gp);
+	        g2.fill(r);
+			
+			graphics.setColor(new Color(0x333333));
+			graphics.drawString(progressInfo.getTypeString(), 2, ((this.getHeight()>>1) + (graphics.getFontMetrics().getHeight()>>1)));
 		}
 	}
 }
