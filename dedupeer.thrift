4b716137829bcf920c6cbccf579bb0e57b9a5c5a
Paulo Fernando <pf@paulofernando.net.br>
2013-02-25 23:59:04 -0300

    Mudança na definição da interface

--- dedupeer.thrift
+++ dedupeer.thrift
@@ -3,14 +3,26 @@ namespace java com.dedupeer.thrift
 typedef i32 int
 typedef i32 weakHash
 
-typedef i64 long
 typedef i64 position
 
 typedef string strongHash
 typedef string chunkID
 
-typedef map<weakHash,map<strongHash,chunkID>> chunksToCompare
+typedef map<weakHash,map<strongHash,chunkID>> hashesToCompare
+
+struct Chunk {
+	1:string fileID,
+	2:string chunkNumber,
+	3:string md5,
+	4:string adler32,
+	5:string index,
+	6:string length,
+	7:string pfile,
+	8:string pchunk, 
+	9:string destination
+	//10:binary content
+}
 
 service DeduplicationService {	
-	map<position,chunkID> deduplicate(1:chunksToCompare chunks, 2:string pathOfFile),
+	map<position,Chunk> deduplicate(1:hashesToCompare chunksInfo, 2:string pathOfFile, 3:int chunkSizeInBytes, 4:int bytesToLoadByTime),
 }
