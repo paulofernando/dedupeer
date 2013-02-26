0270fd172be7e2811ec11d9482d4fe88451b45b3
Paulo Fernando <pf@paulofernando.net.br>
2013-02-25 21:09:38 -0300

    Adição das classes thrift

---
+++ src/com/dedupeer/thrift/ThriftClient.java
@@ -0,0 +1,39 @@
+package com.dedupeer.thrift;
+
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
+
+public class ThriftClient {
+	
+	private void invoke() {
+
+		TTransport transport;
+
+		try {
+			transport = new TSocket("localhost", 7911);
+			TProtocol protocol = new TBinaryProtocol(transport);
+
+			DeduplicationService.Client client = new DeduplicationService.Client(protocol);
+
+			transport.open();
+			long addResult = client.add(100, 200);
+			System.out.println("Add result: " + addResult);
+			long multiplyResult = client.multiply(20, 40);
+			System.out.println("Multiply result: " + multiplyResult);
+			transport.close();
+		} catch (TTransportException e) {
+			e.printStackTrace();
+		} catch (TException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		ThriftClient c = new ThriftClient();
+		c.invoke();
+	}
+}
--- src/com/dedupeer/thrift/ThriftServer.java
+++ src/com/dedupeer/thrift/ThriftServer.java
@@ -7,16 +7,18 @@ import org.apache.thrift.transport.TTransportException;
 
 public class ThriftServer {
 	
+	private int port = 7911;
+	
 	private void start() {
 		try {
-			TServerSocket serverTransport = new TServerSocket(7911);
+			TServerSocket serverTransport = new TServerSocket(port);
 
 			DeduplicationService.Processor processor = new DeduplicationService.Processor(
 					new DeduplicationServiceImpl());
 			TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).
 
 			processor(processor));
-			System.out.println("Starting server on port 7911 ...");
+			System.out.println("Starting server on port " + port + " ...");
 			server.serve();
 		} catch (TTransportException e) {
 			e.printStackTrace();
